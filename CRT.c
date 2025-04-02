fcfb9466c995e3a373e7dc3e6ba9c97c4c88fb74
Sankalp Rawat <sankalprawat2000@gmail.com>
2025-04-02 10:43:47 +0530

    New code

---
+++ CRT.c
@@ -0,0 +1,101 @@
+#include <stdio.h> 
+// int main() { 
+// int i; 
+// for(i = 0; i < 3; ++i) { 
+// printf("%d ", i); 
+// } 
+// return 0; 
+// }
+ 
+// int main() { 
+//     int x = 2; 
+//     switch (x) { 
+//         case 1: 
+//             printf("One "); 
+//             break; 
+//         case 2: 
+//             printf("Two "); 
+//         case 3: 
+//             printf("Three "); 
+//         default: 
+//             printf("Default "); 
+//     } 
+//     return 0; 
+// }
+ 
+// int main() { 
+//     int x = 3; 
+//     switch (x) { 
+//         case 1: 
+//             printf("One "); 
+//             break; 
+//         case 2: 
+//             printf("Two "); 
+//         case 3: 
+//             printf("Three "); 
+//         default: 
+//             printf("Default "); 
+//     } 
+//     return 0; 
+// // } 
+
+// int main() { 
+//     char grade = 'B'; 
+//     switch (grade) { 
+//         case 'A': 
+//             printf("Excellent "); 
+//             break; 
+//         case 'B': 
+//             printf("Good "); 
+//         case 'C': 
+//             printf("Fair "); 
+//         default: 
+//             printf("Average "); 
+//     } 
+//     return 0; 
+// }
+
+// int main() { 
+// int x = 5; 
+// if (x = -5) 
+// printf("Hello"); 
+// else 
+// printf("World"); 
+// printf("%d");
+// return 0; 
+// }
+ 
+// int main() { 
+// int x = 5; 
+// while (x-- > 0) 
+// printf("%d ", x); 
+// return 0; 
+// }
+
+// int main() { 
+//     int i, j; 
+//     for (i = 0; i < 3; i++) { 
+//     for (j = 0; j < 2; j++) { 
+//     printf("%d%d ", i, j); 
+//     } 
+//     } 
+//     return 0; 
+//     }
+
+// int main () {
+//     int x = 255; 
+//     // x+= (x++)+(++x)+x;
+//     printf("%c",x);
+// }
+
+// int main () {
+//     int a = 10,b=20;
+//     if (a>b);
+//     printf("C programming");
+//     return 0 ;
+// }
+
+int main() {
+    
+    return 0 ;
+}
---
+++ helloworld.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main () {
+   for (int i = 1; i <= 5; i++)
+   {
+    for (int j = 1; j<=; j--) {
+        printf(" ");
+
+    }
+    for(int k=1;k<=i;k++){
+        printf("*");
+    }
+    printf("\n");
+   }
+    return 0 ;
+
+}
